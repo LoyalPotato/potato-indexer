fda4e901d93471cc73b78a953a489af4ddedd1a0
David Bazílio Ribeiro <david.bazilio@learninghubz.com>
2024-01-20 19:21:37 +0000

    refactor: Formatting

--- src/settings/settings.ts
+++ src/settings/settings.ts
@@ -4,40 +4,53 @@ import { DEFAULT_SETTINGS } from "./model";
 
 
 export class IndexerSettingsTab extends PluginSettingTab {
-  plugin: Indexer;
+    plugin: Indexer;
 
-  constructor(app: App, plugin: Indexer) {
-    super(app, plugin);
-    this.plugin = plugin;
-  }
+    constructor(app: App, plugin: Indexer) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
 
-  display() {
-    const { containerEl } = this;
+    display() {
+        const { containerEl } = this;
 
-    containerEl.empty();
+        containerEl.empty();
 
-    new Setting(containerEl)
-      .setName("Header title")
-      .setDesc(`What title the index will look for to perform updates on. The default value is: ${DEFAULT_SETTINGS.headerTitleToLookFor}`)
-      .addText((text) =>
-        text
-          .setPlaceholder("## Example")
-          .setValue(this.plugin.settings.headerTitleToLookFor)
-          .onChange(async (value) => {
-            this.plugin.settings.headerTitleToLookFor = value || DEFAULT_SETTINGS.headerTitleToLookFor;
-            await this.plugin.saveSettings();
-          })
-      );
+        new Setting(containerEl)
+            .setName("Header title")
+            .setDesc(
+                `What title the index will look for to perform updates on. The default value is: ${DEFAULT_SETTINGS.headerTitleToLookFor}`,
+            )
+            .addText((text) =>
+                text
+                    .setPlaceholder("## Example")
+                    .setValue(this.plugin.settings.headerTitleToLookFor)
+                    .onChange((val) =>
+                        this.updateSetting(
+                            "headerTitleToLookFor",
+                            val || DEFAULT_SETTINGS.headerTitleToLookFor,
+                        ),
+                    ),
+            );
 
-    new Setting(containerEl)
-      .setName("Insert after first header")
-      .setDesc("Toggle to change insert to the top of the note")
-      .addToggle((toggle) =>
-        toggle.setValue(this.plugin.settings.insertAfterFirstHeader)
-          .onChange(async (value) => {
-            this.plugin.settings.insertAfterFirstHeader = value;
-            await this.plugin.saveSettings();
-          })
-      );
-  }
+        new Setting(containerEl)
+            .setName("Insert after first header")
+            .setDesc("Toggle to change insert to the top of the note")
+            .addToggle((toggle) =>
+                toggle
+                    .setValue(this.plugin.settings.insertAfterFirstHeader)
+                    .onChange((val) =>
+                        this.updateSetting("insertAfterFirstHeader", val),
+                    ),
+            );
+
+
+    private async updateSetting<S extends keyof IndexerSettings>(
+        setting: S,
+        value: IndexerSettings[S],
+    ) {
+        this.plugin.settings[setting] = value;
+        await this.plugin.saveSettings();
+    }
+
 }
