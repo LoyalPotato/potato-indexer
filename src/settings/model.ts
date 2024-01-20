c37165b3a614a11fadc4c78acb274a1b8ac59616
David Bazílio Ribeiro <david.bazilio@learninghubz.com>
2024-01-20 19:18:45 +0000

    feat: Add new settings
    issue #12

--- src/settings/model.ts
+++ src/settings/model.ts
@@ -1,9 +1,14 @@
 export interface IndexerSettings {
-  insertAfterFirstHeader: boolean;
-  headerTitleToLookFor: string;
+    insertAfterFirstHeader: boolean;
+    headerTitleToLookFor: string;
+    minHeader: number;
+    maxHeader: number;
 }
 
 export const DEFAULT_SETTINGS: IndexerSettings = {
-  headerTitleToLookFor: "## Content Index",
-  insertAfterFirstHeader: true
+    headerTitleToLookFor: "## Content Index",
+    insertAfterFirstHeader: true,
+    minHeader: 1,
+	maxHeader: 6
 };
+
