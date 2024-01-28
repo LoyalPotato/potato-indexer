37b19335efcb44a9380db8751a99458386391659
David Bazílio Ribeiro <david.bazilio@learninghubz.com>
2024-01-28 15:29:49 +0000

    feat: Add func to concat lines

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -6,3 +6,14 @@ export function checkHeaderSize(s: string, min: number, max: number): boolean {
 
     return i >= min && i <= max;
 }
+export function concatLines(lines: string[], isPostContent = false): string {
+    let s = "";
+    for (let i = 0; i < lines.length; ++i) {
+        s += lines[i];
+        if (!isPostContent || i < lines.length - 1) {
+            s += "\n";
+        }
+    }
+
+    return s;
+}
